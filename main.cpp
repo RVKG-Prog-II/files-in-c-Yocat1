f007ef7db873ea0fd038dd93cf732126c3e7acb3
Yocat1 <162314939+Yocat1@users.noreply.github.com>
2024-03-19 13:42:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,55 @@
 #include <string>
-// You may include other headers, as needed
+#include <iostream>
+#include <fstream>
 
 using std::string;
+using namespace std;
 
 void countAndWriteDigitsPerLine(string filename, string outputFilename)
 {
-    // Your code here
+    int counter = 0;
+    string myText;
+    ifstream Read(filename);
+    ofstream Write(outputFilename);
+    while (getline(Read, myText)) {
+        for (int i = 0; i < myText.size(); i++) {
+            if (isdigit(myText[i])) {
+                counter++;
+            }
+        }
+        Write << counter;
+        Write << "\n";
+        counter = 0;
+    }
+
+   
+    Read.close();
+    Write.close();
 }
 
 unsigned int countMaxDigits(string filename)
 {
-    // Your code here
+    int counter = 0;
+    int max = 0;
+    string myText;
+    ifstream Read(filename);
+    while (getline(Read, myText)) {
+        for (int i = 0; i < myText.size(); i++) {
+            if (isdigit(myText[i])) {
+                counter++;
+            }
+        }
+        if (counter > max) {
+            max = counter;
+        }
+        counter = 0;
+    }
+
+
+    Read.close();
+    return max;
+}
+int main() {
+    countAndWriteDigitsPerLine("filename.txt", "outputFilename.txt");
+    cout << countMaxDigits("filename.txt");
 }
